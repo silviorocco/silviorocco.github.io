632a7c74d94d3e04cf1ac91ebc93d139ab7f4f97
silviorocco <silviorocco9@gmail.com>
2024-11-08 21:21:15 -0400

    Add files via upload

---
+++ baseDePreguntas.js
@@ -0,0 +1,22 @@
+baseDePreguntas = [
+  {
+    pregunta: `¿Qué es un hiato?`,
+    imagen: `https://i.ytimg.com/vi/VMJi53lnYoM/maxresdefault.jpg`,
+    respuesta: `Un hiato es la separación de dos vocales que pertenecen a sílabas diferentes, donde ambas son vocales abiertas (a, e, o) o una vocal cerrada acentuada (í, ú) y una vocal abierta. `,
+    distractores: [`Un hiato es la unión de dos vocales en la misma sílaba.`, `Un hiato es un tipo de signo de puntuación. `, `Un hiato es una palabra que no tiene significado.`],
+    ayuda: `Imagina que tienes dos vocales que quieren estar en diferentes casas (sílabas). Si ambas son vocales abiertas o una cerrada acentuada se separan, eso es un hiato. ¡No se juntan en la misma sílaba!`,
+    ayudaImg: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3qiFLbGe7o-6AHbYdz4_CKrInM1YhJkLahw&s`,
+  },
+  {
+    pregunta: `¿Qué son las palabras homófonas? `,
+    respuesta: `Las palabras homófonas son aquellas que suenan igual pero tienen diferentes significados y, a veces, diferentes ortografías. `,
+    distractores: [`Las palabras homófonas son aquellas que se escriben igual y tienen el mismo significado. `, `Las palabras homófonas son palabras que riman entre sí. `, `Las palabras homófonas son palabras que tienen un significado opuesto.`],
+    ayuda: `Piensa en dos palabras que suenan igual, como si fueran gemelas, pero tienen significados diferentes. A veces, incluso se escriben de manera distinta. ¡Esa es la esencia de las palabras homófonas!`,
+    ayudaImg: `https://lenguaje.com/wp-content/uploads/2023/07/PALABRAS-HOMOFONAS.png`,
+  },
+  {
+    pregunta: `¿Qué es un diptongo?`,
+    respuesta: `Un diptongo es la combinación de dos vocales en la misma sílaba, donde al menos una de ellas es una vocal cerrada (i, u). `,
+    distractores: [`Un diptongo es una palabra que tiene tres sílabas.`, `Un diptongo es un tipo de adjetivo. `, `Un diptongo es una forma de puntuación.`],
+  }
+];
